c5b9943d9c37bc3bac252bfe076afb9dae68d268
hyee0715 <hyee0715@naver.com>
2023-07-15 01:27:53 +0900

    Initial commit

---
+++ src/main/java/com/project/nexedu/NexEduApplication.java
@@ -0,0 +1,13 @@
+package com.project.nexedu;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NexEduApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(NexEduApplication.class, args);
+    }
+
+}
