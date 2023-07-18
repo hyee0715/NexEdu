a986b74d05202fc23f264168959ca687b9b7ea4b
hyee0715 <hyee0715@naver.com>
2023-07-18 09:20:40 +0900

    feat: 댓글 rest api 기능 구현

---
+++ src/main/java/com/project/nexedu/domain/comment/CommentRepository.java
@@ -0,0 +1,9 @@
+package com.project.nexedu.domain.comment;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface CommentRepository extends JpaRepository<Comment, Long> {
+
+}
